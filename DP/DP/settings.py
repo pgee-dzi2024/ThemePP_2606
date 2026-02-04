af5758698e902370464ee36fb2bfb160cfc394e4
pgee-dzi2024 <pgee_dzi2024@abv.bg>
2026-02-04 11:16:20 +0200

    Чиста инсталация на Django.

--- DP/DP/settings.py
+++ DP/DP/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!^reer%vw9@mket#z_euucw*nkuqwf414a#)6@3$7fb3s9!w$d'
+SECRET_KEY = 'django-insecure-b5l$(g6dk!7vuplsw7a_^u+-z920%8n*$t5bzkx)-!z%9xh=%j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'bg'
 
 TIME_ZONE = 'UTC'
 
